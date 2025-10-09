d9bfd22ab914ddd1be0e621144b3bda9a8e9ce9d
Shubham Patel <sanket13052004@gmail.com>
2025-10-09 16:21:40 +0530

    paramertic urls added

--- app/tools/category/[slug]/page.tsx
+++ app/tools/category/[slug]/page.tsx
@@ -1,6 +1,11 @@
 // app/tools/category/[slug]/page.tsx
 import { ToolsContent } from "@/app/tools/ToolsContent";
 
-export default function ToolsByCategory({ params }: { params: { slug: string } }) {
-  return <ToolsContent categorySlug={params.slug} />;
+export default async function ToolsByCategory({
+  params,
+}: {
+  params: { slug: string } | Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return <ToolsContent categorySlug={slug} />;
 }
